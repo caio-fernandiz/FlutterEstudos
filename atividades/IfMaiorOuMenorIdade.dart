aee75fdfa7a1444cbeb4f5c40dae1d66c3957319
caio-fernadiz <arostuche@gmail.com>
2025-01-31 11:53:17 -0300

    Flutter Estudos

--- atividades/IfMaiorOuMenorIdade.dart
+++ atividades/IfMaiorOuMenorIdade.dart
@@ -4,6 +4,14 @@ void main(List<String> args) {
   int idade;
   String? escolha;
   do {
+    idade = int.parse(stdin.readLineSync()!);
+
+    if (idade < 18) {
+      print("Menor de idade");
+    } else {
+      print("Maior de idade");
+    }
+
     stdout.write("Deseja continuar? Y/N: ");
     escolha = stdin.readLineSync();
   } while (escolha != "N" && escolha != "n");
