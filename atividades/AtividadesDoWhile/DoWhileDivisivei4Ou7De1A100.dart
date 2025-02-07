f0987577e02522cdd24112e15993bea0d69f408e
caio-fernadiz <arostuche@gmail.com>
2025-02-07 11:06:44 -0300

    Estudos Dart

--- atividades/AtividadesDoWhile/DoWhileDivisivei4Ou7De1A100.dart
+++ atividades/AtividadesDoWhile/DoWhileDivisivei4Ou7De1A100.dart
@@ -2,13 +2,15 @@ void main(List<String> args) {
   int i = 1;
 
   do {
-    i++;
-    if (i % 4 == 0) {
+    if (i % 4 == 0 && i % 7 == 0) {
+      print("o número $i é divisivel por 4 e por 7");
+    } else if (i % 4 == 0) {
       print("o número $i é divisivel por 4");
     } else if (i % 7 == 0) {
       print("o número $i é divisivel por 7");
     } else {
       print("o número $i não é divisivel nem por 4 e nem por 7");
     }
+    i++;
   } while (i <= 100);
 }
