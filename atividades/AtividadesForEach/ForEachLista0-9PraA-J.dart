66780c376fb164ff88c89bb81c0cad360a4a8e01
caio-fernadiz <arostuche@gmail.com>
2025-02-13 08:25:48 -0300

    estudos dart

--- atividades/AtividadesForEach/ForEachLista0-9PraA-J.dart
+++ atividades/AtividadesForEach/ForEachLista0-9PraA-J.dart
@@ -1,3 +1,7 @@
 void main(List<String> args) {
-  List<int> numeros;
+  List<int> numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  numeros.forEach((codeChar) {
+    print(String.fromCharCode(65 + codeChar));
+  });
 }
